a667b42104acd19d10a48f2ff52672593635f8b1
A W <waq.mmhy@live.ca>
2019-07-19 13:20:35 -0400

    Initial Commit

---
+++ Project 16 - Capital Cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 16 - Capital Cities
+//
+//  Created by Angela Wu  on 2019-07-19.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
